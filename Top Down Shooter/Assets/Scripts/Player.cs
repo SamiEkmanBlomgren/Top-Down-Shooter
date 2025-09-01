2759adf3bab1ee2004887885c1e241273629c024
SamiEkmanBlomgren <sami.ekmanblomgren@elev.ga.lbs.se>
2025-09-01 11:01:46 +0200

    Added Player

---
+++ Top Down Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 moveInput;
+    [SerializeField] float moveSpeed = 3;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+}
