386dec9f82a4d20d8dbdab9ae461e4d991dd6859
SamiEkmanBlomgren <sami.ekmanblomgren@elev.ga.lbs.se>
2025-09-08 11:00:43 +0200

    Added player bullet and collision, enemy movement

---
+++ Top Down Shooter/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float moveSpeed = 3f;
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+    }
+}
--- Top Down Shooter/Assets/Scripts/Player.cs
+++ Top Down Shooter/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     Vector2 moveInput;
     [SerializeField] float moveSpeed = 3;
+    [SerializeField] float bulletSpeed = 7f;
+    [SerializeField] GameObject bullet; 
 
 
 
@@ -18,6 +20,13 @@ public class Player : MonoBehaviour
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    void OnAttack()
+    {
+        Rigidbody2D playerBullet = Instantiate(bullet, transform.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+        playerBullet.AddForce(transform.up * bulletSpeed, ForceMode2D.Impulse);
+
     }
 
     // Update is called once per frame
