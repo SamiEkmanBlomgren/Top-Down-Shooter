207dc39ce708cbeff14870bd56318153d8cba8ec
SamiEkmanBlomgren <sami.ekmanblomgren@elev.ga.lbs.se>
2025-09-22 11:03:41 +0200

    Enemy spawn + damage

--- Top Down Shooter/Assets/Scripts/EnemyScript.cs
+++ Top Down Shooter/Assets/Scripts/EnemyScript.cs
@@ -2,19 +2,23 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    [SerializeField] Transform player;
+    Transform player;
     [SerializeField] float moveSpeed = 3f;
     Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 direction = (player.position - transform.position).normalized;
-        rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+        if (player != null)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+        }
     }
 }
--- Top Down Shooter/Assets/Scripts/Player.cs
+++ Top Down Shooter/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,13 +6,16 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 moveInput;
+    Vector2 screenBoundary;
     [SerializeField] float moveSpeed = 3;
     [SerializeField] float bulletSpeed = 7f;
     [SerializeField] GameObject bullet;
     [SerializeField] GameObject gun;
     [SerializeField] float rotationSpeed = 700f;
-
+    int playerHealth = 3;
     float targetAngle;
+    SpriteRenderer playerSprite;
+    
 
 
 
@@ -19,6 +23,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        screenBoundary = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 
     void OnMove(InputValue value)
@@ -40,10 +45,31 @@ public class Player : MonoBehaviour
         {
             targetAngle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg;
         }
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -screenBoundary.x, screenBoundary.x)
+                                        , Mathf.Clamp(transform.position.y, -screenBoundary.y, screenBoundary.y));
     }
     private void FixedUpdate()
     {
         float rotation = Mathf.MoveTowardsAngle(rb.rotation, targetAngle - 90, rotationSpeed * Time.fixedDeltaTime);
         rb.MoveRotation(rotation);
+    }
+    IEnumerator IFrame()
+    {
+        playerSprite.color = Color.darkRed;
+        yield return new WaitForSeconds(1);
+        playerSprite.color = Color.red;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemies"))
+        {
+            StartCoroutine("IFrame");
+            playerHealth -= 1;
+        }
+        if (playerHealth == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
